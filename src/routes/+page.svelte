4f9d5e6a7a09fcaeee065c1656ab0894fe325ce7
Chris Putnam <chris@webfx.com>
2024-03-29 10:02:20 -0400

    Input feedback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,80 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let data;
 
+	// TODO switch images to just fade out/in and only show one at a time vs. extra html
+
+	let html_feedback = 'Feedback';
 	let person = null;
+	let name_entered_by_user = '';
+	let el_input_name;
 
 	function showRandomPerson() {
 		const randomIndex = Math.floor(Math.random() * data.people.length);
 		person = data.people[randomIndex];
 	}
 
-	showRandomPerson();
+	function giveFeedback(feedback = '') {
+		html_feedback = feedback;
+	}
 
-	// TODO switch images to just fade out/in and only show one at a time vs. extra html
+	function handleInputKeys(event = {}) {
+		if (name_entered_by_user === '') {
+			return giveFeedback('Type name and press enter');
+		}
+
+		if (!('key' in event)) {
+			return;
+		}
+
+		if (event.key === 'Esc') {
+			// TODO implement
+			return giveFeedback('Not yet implemented');
+		}
+
+		if (event.key !== 'Enter') {
+			return;
+		}
+
+		if (name_entered_by_user === person.name) {
+			return giveFeedback('Exactly!<br>Press enter again to continue.');
+		}
+
+		let feedback = 'Incorrect.';
+		if (name_entered_by_user.toLowerCase() === person.name.toLowerCase()) {
+			feedback = 'Close! But, the case is not quite right.';
+		}
+
+		giveFeedback(feedback + '<br>Try again or hit Esc to see name.');
+	}
+
+	showRandomPerson();
+	handleInputKeys();
+
+	onMount(() => {
+		el_input_name.focus();
+	});
 </script>
 
 <div class="background">
 	<div class="quiz-container">
-		<div class="img-slider">
-			<div class="img-slider-strip">
-				{#each person.photos as photo}
-					<img src={photo} alt={person.name} />
-				{/each}
+		<div class="quiz-content">
+			<div class="img-slider">
+				<div class="img-slider-strip">
+					{#each person.photos as photo}
+						<img src={photo} alt="A randomly selected person" />
+					{/each}
+				</div>
 			</div>
+			<input
+				type="text"
+				placeholder="Type name and press enter"
+				on:keyup={handleInputKeys}
+				bind:value={name_entered_by_user}
+				bind:this={el_input_name}
+			/>
+			<div class="feedback">{@html html_feedback}</div>
 		</div>
 	</div>
 </div>
@@ -36,8 +90,15 @@
 
 	.quiz-container {
 		background-color: #eee;
-		padding: 20px;
+		padding: 40px 20px;
+		width: 300px;
 		border-radius: 10px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.quiz-content {
 		justify-content: center;
 		align-items: center;
 	}
@@ -53,5 +114,9 @@
 
 	.img-slider img {
 		width: 200px;
+	}
+
+	input {
+		margin: 10px 0;
 	}
 </style>
