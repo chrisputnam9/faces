ca43973b3b688e18d07fa55b7bded6c03ddb40a0
Chris Putnam <chris@webfx.com>
2024-04-03 09:04:01 -0400

    Sample people update

--- data/people-sample.js
+++ data/people-sample.js
@@ -1,8 +1,11 @@
 export const people = [
 	{
 		name: 'John Smith',
+		first_name: 'John',
+		middle_name: '',
+		last_name: 'Smith',
 		aliases: ['Jo-Jo', 'Jay', 'The Smithster'],
-		photos: ['http://example.com/john.jpg', 'http://example.com/john2.jpg'],
+		images: ['http://example.com/john.jpg', 'http://example.com/john2.jpg'],
 		profiles: ['http://example.com/john', 'http://example.com/john2'],
 		companies: ['Acme', 'Globex'],
 		emails: ['jsmith@example.com', 'john@example.com'],
@@ -10,8 +13,11 @@ export const people = [
 	},
 	{
 		name: 'Jane Doe',
+		first_name: 'Jane',
+		middle_name: '',
+		last_name: 'Doe',
 		aliases: ['Janey', 'Doe-Doe'],
-		photos: ['http://example.com/jane.jpg', 'http://example.com/jane2.jpg'],
+		images: ['http://example.com/jane.jpg', 'http://example.com/jane2.jpg'],
 		profiles: ['http://example.com/jane', 'http://example.com/jane2'],
 		companies: ['BCD Inc.', 'Globex'],
 		emails: ['jdoe@example.com'],
