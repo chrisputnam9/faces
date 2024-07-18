7c070913449b655516c752eaa94d2145e3f3e834
Chris Putnam <chris@webfx.com>
2024-07-18 09:00:13 -0400

    indexeddb in progress

--- src/lib/stores/createIndexedDBStore.ts
+++ src/lib/stores/createIndexedDBStore.ts
@@ -4,18 +4,48 @@
 
 import { writable } from 'svelte/store';
 
-// Listen for changes to local storage and update registered stores
-// TODO
+// TODO - Add a way to listen for changes using local storage to help
 
-export function createIndexedDBStore (key, default_value=null) {
+export async function createIndexedDBStore (key, identifier='id', default_value=null) {
 
 	const { subscribe, set, update } = writable(default_value);
 
 	if (typeof window === 'undefined') {
 		throw new Error('IndexedDB storage can only be used in the browser');
 	}
+	
+	function error_handler(event) {
+		const message = 'Error 201: Problem with IndexedDB database - see console for more details');
+		alert(message);
+		console.error(message, event);
+	}
 
-	// TODO Switch over to IndexedDB
+	// Open IndexedDB database and set up error handling
+	const db = await new Promise(function (resolve) {
+		const db_open_request = window.indexedDB.open('application', 1);
+		db_open_request.onerror = error_handler;
+		db_open_request.onsuccess = (event) => {
+			resolve(event.target.result);
+		}
+	});
+	db.onerror = error_handler;
+
+	// Initialize Object Store and wait for that to be complete
+	const object_store = new Promise(function (resolve) {
+		db.createObjectStore(key, { keyPath: identifier });
+		transaction.oncomplete = resolve;
+	});
+
+	function transation_promise () {
+		return new Promise((resolve) => {
+			const transaction = db.transaction([key], 'readwrite');
+			transaction.onerror = error_handler;
+			transaction.oncomplete = () => {
+				resolve();
+			};
+		});
+	
+	}
 
 	// Prime the store with the value from local storage, if any
 	const local_value = localStorage.getItem(key);
@@ -25,10 +55,6 @@ export function createIndexedDBStore (key, default_value=null) {
 		// If nothing in local storage, set the default value
 		localStorage.setItem(key, JSON.stringify(default_value));
 	}
-
-	// Register this store for updates to local storage
-	//  - this way, when we subscribe to changes, we get changes from other tabs
-	// TODO - we could use local storage to send events - eg. keys with updated_at...
 
 	return {
 		subscribe,
