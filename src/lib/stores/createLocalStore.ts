d3a3c0f351589173a17afb5fa8657490fedfe657
Chris Putnam <chris@webfx.com>
2024-09-12 08:22:48 -0400

    Adjust local storage store by storing data as JSON

--- src/lib/stores/createLocalStore.ts
+++ src/lib/stores/createLocalStore.ts
@@ -2,7 +2,7 @@
  * Use this function to create a new local storage store
  */
 
-import { get, writable } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 // Listen for changes to local storage and update registered stores
 // TODO
@@ -15,37 +15,36 @@ export function createLocalStore (key, default_value=null) {
 		throw new Error('Local storage can only be used in the browser');
 	}
 
+	const store_interface = {
+		subscribe: function (callback, invalidate=null) {
+			return subscribe(function (value_string) {
+				// Parse the JSON since that's how we store the value - see '.set'
+				const value = JSON.parse(value_string);
+				return callback(value);
+			}, invalidate);
+		},
+		set: function (value) {
+			const value_string = JSON.stringify(value);
+			console.log('LocalStore.set:', key, value_string);
+			// We store the value as a JSON string in the store as well
+			// to avoid issues with updates via other references to the object
+			localStorage.setItem(key, value_string);
+			set(value_string);
+		},
+		update: function (callback) {
+			console.log('LocalStore.update:', key, value);
+			const local_value = localStorage.getItem(key);
+			const updated_value = callback(JSON.parse(local_value));
+			this.set(updated_value);
+		}
+	};
+
 	// Prime the store with the value from local storage, if any
 	const local_value_raw = localStorage.getItem(key);
 
-	console.log({
-		local_value_raw,
-		parsed: JSON.parse(local_value_raw),
-	});
-
 	if (local_value_raw !== null) {
-
-		console.log({
-			local_value_raw,
-			parsed: JSON.parse(local_value_raw),
-		});
-
 		const local_value = JSON.parse(local_value_raw);
-
-		console.log({
-			local_value_raw,
-			parsed: JSON.parse(local_value_raw),
-			local_value
-		});
-
-		set(local_value);
-
-		console.log({
-			local_value_raw,
-			parsed: JSON.parse(local_value_raw),
-			local_value
-		});
-
+		store_interface.set(local_value);
 		if (key === 'sync') {
 			console.log('Localstore sync loaded from local storage:', local_value_raw, local_value);
 		}
@@ -57,25 +56,9 @@ export function createLocalStore (key, default_value=null) {
 		}
 	}
 
-	console.log('-- store ', key, get({subscribe}));
-
-	// Register this store for updates to local storage?
+	// TODO - Register this store for updates to local storage?
 	// In theory this could allow for useful multi-tab behaviors
 	// Might need a lot of testing
-	// TODO
 
-	return {
-		subscribe,
-		set: function (value) {
-			console.log('LocalStore.set:', key, value);
-			localStorage.setItem(key, JSON.stringify(value));
-			set(value);
-		},
-		update: function (callback) {
-			console.log('LocalStore.update:', key, value);
-			const local_value = localStorage.getItem(key);
-			const updated_value = callback(JSON.parse(local_value));
-			this.set(updated_value);
-		}
-	}
+	return store_interface;
 }
