21a68006c61e9cde0a7e6c3af27dfcdef46ce2a4
Chris Putnam <chris@webfx.com>
2024-11-21 08:25:59 -0500

    AI help tweaks

--- src/lib/components/PersonDetails.svelte
+++ src/lib/components/PersonDetails.svelte
@@ -10,6 +10,7 @@
 	let ai_help_by_word = {};
 	let ai_help_in_progress = false;
 	let ai_help_error = false;
+	let ai_init_error = false;
 
 	async function get_ai_help(word) {
 		const fresh = word in ai_help_by_word;
@@ -35,26 +36,76 @@
 				.replace(/[^a-z0-9-]+/g, '')
 				.split('-')
 		: [];
+
+	// Try to initialize AI
+	aiInterface.init().catch((e) => {
+		ai_init_error = e.message;
+	});
 </script>
 
 <div class="details state-guess-{state_guess}">
 	<h1>
 		{person.name}
-		<br />
-		{#if person.companies.length > 0}<small>({person.companies.join(', ')})</small>{/if}
+		{#if person.companies.length > 0}<br /><small>({person.companies.join(', ')})</small>{/if}
 	</h1>
 
-	<h2>AI Help:</h2>
-	<p>Use experimental AI built into Chrome to help find associations with a person's name.</p>
-	{#if ai_help_error}
-		<p><b>AI Assistance error</b> - {ai_help_error}</p>
+	{#each person.facts as fact}
+		<br /><b>{fact.name}:</b> {fact.value}
+	{/each}
+	{#each person.emails as email}
+		<br /><b>Email:</b> <a href="mailto:{email}" target="_blank">{email}</a>
+	{/each}
+
+	{#if person.description}
+		<!-- Trusted user input only used by the user -->
+		<!-- eslint-disable svelte/no-at-html-tags -->
+		<br />{@html person.description.replace(/\n/g, '<br />')}
 	{/if}
-	{#each person_name_words as word}
-		<h3>{word}</h3>
-		{#if word in ai_help_by_word}
-			<pre>{ai_help_by_word[word]}</pre>
-		{/if}
+
+	{#each person.links as link}
+		<br /><a href={link.url} target="_blank">{link.text}</a>
+	{/each}
+	<br /><a href="https://www.google.com/search?q=name meaning {person.first_name}" target="_blank">
+		Meaning of name "{person.first_name}"
+	</a>
+	<br /><a href="https://www.google.com/search?q=name meaning {person.last_name}" target="_blank">
+		Meaning of name "{person.last_name}"
+	</a>
+	<br /><a href="https://www.facebook.com/search/top/?q={person_company_search}" target="_blank">
+		Facebook
+	</a>
+	<br /><a
+		href="https://www.linkedin.com/search/results/all/?keywords={person_company_search}"
+		target="_blank"
+	>
+		LinkedIn
+	</a>
+	<br /><a href="https://contacts.google.com/search/{person_search}" target="_blank">
+		Google Contact Directory
+	</a>
+
+	<br />
+	<br />
+	<h2>AI Help:</h2>
+	{#if ai_init_error}
 		<p>
+			{ai_init_error}
+			<br /><a href="/aitest">Click here to learn more and setup AI help</a>
+		</p>
+	{:else}
+		<br />Use experimental AI built into Chrome
+		<br />to help find associations with a person's name.
+		{#if ai_help_error}
+			<p>
+				<b class="error">AI Assistance error</b> - {ai_help_error}
+				<br /><a href="/aitest">Click here for more options</a>
+			</p>
+		{/if}
+		{#each person_name_words as word}
+			<h3>{word}</h3>
+			{#if word in ai_help_by_word}
+				<pre>{ai_help_by_word[word]}</pre>
+			{/if}
 			<button on:click={() => get_ai_help(word)} disabled={ai_help_in_progress}>
 				{#if ai_help_in_progress}
 					Working...
@@ -64,59 +115,8 @@
 					Help me remember '{word}'
 				{/if}
 			</button>
-		</p>
-	{/each}
-	<hr />
-
-	<p>
-		{#each person.facts as fact}
-			<br /><b>{fact.name}:</b> {fact.value}
 		{/each}
-		{#each person.emails as email}
-			<br /><b>Email:</b> <a href="mailto:{email}" target="_blank">{email}</a>
-		{/each}
-	</p>
-
-	<p>
-		<!-- Trusted user input only used by the user -->
-		<!-- eslint-disable svelte/no-at-html-tags -->
-		{@html person.description.replace(/\n/g, '<br />')}
-	</p>
-
-	<b>Links:</b>
-	<ul>
-		{#each person.links as link}
-			<li>
-				<a href={link.url} target="_blank">{link.text}</a>
-			</li>
-		{/each}
-		<li>
-			<a href="https://www.google.com/search?q=name meaning {person.first_name}" target="_blank"
-				>Meaning of name "{person.first_name}"</a
-			>
-		</li>
-		<li>
-			<a href="https://www.google.com/search?q=name meaning {person.last_name}" target="_blank"
-				>Meaning of name "{person.last_name}"</a
-			>
-		</li>
-		<li>
-			<a href="https://www.facebook.com/search/top/?q={person_company_search}" target="_blank"
-				>Facebook</a
-			>
-		</li>
-		<li>
-			<a
-				href="https://www.linkedin.com/search/results/all/?keywords={person_company_search}"
-				target="_blank">LinkedIn</a
-			>
-		</li>
-		<li>
-			<a href="https://contacts.google.com/search/{person_search}" target="_blank"
-				>Google Contact Directory</a
-			>
-		</li>
-	</ul>
+	{/if}
 </div>
 
 <style>
@@ -130,9 +130,17 @@
 		display: block;
 	}
 
-	h1 {
+	h1,
+	h2 {
 		font-size: 1.2em;
 		margin: 0;
+		padding: 0;
+		display: inline;
+	}
+	h3 {
+		font-size: 1em;
+		margin: 0;
+		padding: 15px 0 0 0;
 	}
 	pre {
 		white-space: pre-wrap;
@@ -142,5 +150,12 @@
 		border: 1px solid #aaa;
 		width: 100%;
 		height: auto;
+	}
+	button {
+		margin-top: 10px;
+	}
+	.error {
+		color: darkred;
+		text-decoration: underline;
 	}
 </style>
