d3b63d87c148d12db05b570f4bcf46a619f5efd4
Chris Putnam <chris@webfx.com>
2025-06-13 08:38:39 -0400

    Add environment to Sentry tracking

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,6 +5,8 @@ import * as Sentry from '@sentry/sveltekit';
 Sentry.init({
 	dsn: 'https://e5cd2a60d78fb2355c5007fd26ce76a3@o4509315849519104.ingest.us.sentry.io/4509474468397056',
 
+	environment: import.meta.env.MODE,
+
 	tracesSampleRate: .1
 
 	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
